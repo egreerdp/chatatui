c38ff08662b12f038377bdc645e8abeb864bc88e
egreerdp <ewan.greer@dailypay.com>
2025-12-22 15:37:09 +0000

    basic hub routing

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,21 +1,40 @@
 package server
 
-import "github.com/egreerdp/chatatui/internal/server/api"
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/egreerdp/chatatui/internal/server/api"
+)
 
 type ChatServer struct {
 	handler *api.Handler
+	srv     *http.Server
+	addr    string
 }
 
-func NewChatServer(h *api.Handler) *ChatServer {
+func NewChatServer(h *api.Handler, addr string) *ChatServer {
 	return &ChatServer{
 		handler: h,
+		addr:    addr,
 	}
 }
 
 func (cs *ChatServer) Start() error {
-	return nil
+	cs.srv = &http.Server{
+		Addr:    cs.addr,
+		Handler: cs.handler.Router,
+	}
+
+	log.Println("listen and serve")
+	return cs.srv.ListenAndServe()
 }
 
 func (cs *ChatServer) Stop() error {
+	if cs.srv != nil {
+		return cs.srv.Shutdown(context.Background())
+	}
+
 	return nil
 }
