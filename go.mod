3a3b4d7d506aea301d2ef522edcf8308eb3b5fa7
EwanGreer <ewanja.greer@gmail.com>
2025-12-28 17:56:56 +0000

    feat: scaffold client layout

--- go.mod
+++ go.mod
@@ -2,23 +2,29 @@ module github.com/egreerdp/chatatui
 
 go 1.25.5
 
-require github.com/go-chi/chi v1.5.5
+require (
+	github.com/charmbracelet/bubbles v0.21.0
+	github.com/charmbracelet/bubbletea v1.3.10
+	github.com/charmbracelet/lipgloss v1.1.0
+	github.com/coder/websocket v1.8.14
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/google/uuid v1.6.0
+	github.com/spf13/cobra v1.10.2
+	github.com/spf13/viper v1.21.0
+	gorm.io/driver/sqlite v1.6.0
+	gorm.io/gorm v1.31.1
+)
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/bubbles v0.21.0 // indirect
-	github.com/charmbracelet/bubbletea v1.3.10 // indirect
 	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
-	github.com/charmbracelet/lipgloss v1.1.0 // indirect
 	github.com/charmbracelet/x/ansi v0.10.1 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
-	github.com/coder/websocket v1.8.14 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	github.com/go-chi/chi/v5 v5.2.3 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -36,14 +42,10 @@ require (
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
-	github.com/spf13/cobra v1.10.2 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
-	github.com/spf13/viper v1.21.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
-	gorm.io/driver/sqlite v1.6.0 // indirect
-	gorm.io/gorm v1.31.1 // indirect
 )
--- internal/client/ui/index.go
+++ internal/client/ui/index.go
@@ -1,37 +1,214 @@
 package ui
 
 import (
-	"strings"
-
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-type Model struct{}
+type focus int
+
+const (
+	focusRooms focus = iota
+	focusMessages
+	focusInput
+)
+
+type Room struct {
+	ID   string
+	Name string
+}
+
+type Model struct {
+	viewport  viewport.Model
+	input     textinput.Model
+	rooms     []Room
+	messages  []string
+	focus     focus
+	width     int
+	height    int
+	ready     bool
+	roomIndex int
+}
 
 func NewModel() *Model {
-	return &Model{}
+	ti := textinput.New()
+	ti.Placeholder = "Type a message..."
+	ti.Focus()
+
+	return &Model{
+		input:    ti,
+		rooms:    []Room{},
+		messages: []string{},
+		focus:    focusInput,
+	}
 }
 
 func (m Model) Init() tea.Cmd {
-	return nil
+	return textinput.Blink
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmds []tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			return m, tea.Quit
+		case "tab":
+			m.cycleFocus()
+		case "enter":
+			if m.focus == focusInput && m.input.Value() != "" {
+				m.messages = append(m.messages, "You: "+m.input.Value())
+				m.input.Reset()
+				m.updateViewportContent()
+				m.viewport.GotoBottom()
+			}
+		case "up", "k":
+			if m.focus == focusRooms && m.roomIndex > 0 {
+				m.roomIndex--
+			}
+		case "down", "j":
+			if m.focus == focusRooms && m.roomIndex < len(m.rooms)-1 {
+				m.roomIndex++
+			}
 		}
+
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+
+		sidebarWidth := m.sidebarWidth()
+		mainWidth := m.width - sidebarWidth - 1
+
+		headerHeight := 1
+		inputHeight := 3
+		viewportHeight := m.height - headerHeight - inputHeight
+
+		if !m.ready {
+			m.viewport = viewport.New(mainWidth, viewportHeight)
+			m.viewport.SetContent("")
+			m.ready = true
+		} else {
+			m.viewport.Width = mainWidth
+			m.viewport.Height = viewportHeight
+		}
+
+		m.input.Width = mainWidth - 2
 	}
 
-	return m, nil
+	if m.focus == focusInput {
+		var cmd tea.Cmd
+		m.input, cmd = m.input.Update(msg)
+		cmds = append(cmds, cmd)
+	}
+
+	if m.focus == focusMessages {
+		var cmd tea.Cmd
+		m.viewport, cmd = m.viewport.Update(msg)
+		cmds = append(cmds, cmd)
+	}
+
+	return m, tea.Batch(cmds...)
 }
 
 func (m Model) View() string {
-	var sb strings.Builder
+	if !m.ready {
+		return "Loading..."
+	}
 
-	sb.WriteString("\nPress q to quit.\n")
+	sidebar := m.renderSidebar()
+	main := m.renderMain()
 
-	return sb.String()
+	return lipgloss.JoinHorizontal(lipgloss.Top, sidebar, main)
+}
+
+func (m *Model) cycleFocus() {
+	switch m.focus {
+	case focusInput:
+		m.focus = focusRooms
+		m.input.Blur()
+	case focusRooms:
+		m.focus = focusMessages
+	case focusMessages:
+		m.focus = focusInput
+		m.input.Focus()
+	}
+}
+
+func (m Model) sidebarWidth() int {
+	return 20
+}
+
+func (m Model) renderSidebar() string {
+	width := m.sidebarWidth()
+
+	style := lipgloss.NewStyle().
+		Width(width).
+		Height(m.height).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderRight(true).
+		Padding(0, 1)
+
+	if m.focus == focusRooms {
+		style = style.BorderForeground(lipgloss.Color("62"))
+	}
+
+	header := lipgloss.NewStyle().Bold(true).Render("Rooms")
+
+	var roomList string
+	for i, room := range m.rooms {
+		name := room.Name
+		if i == m.roomIndex {
+			name = "> " + name
+		} else {
+			name = "  " + name
+		}
+		roomList += name + "\n"
+	}
+
+	if len(m.rooms) == 0 {
+		roomList = "(no rooms)"
+	}
+
+	content := header + "\n\n" + roomList
+
+	return style.Render(content)
+}
+
+func (m Model) renderMain() string {
+	headerStyle := lipgloss.NewStyle().
+		Bold(true).
+		Padding(0, 1)
+
+	header := headerStyle.Render("chatatui")
+
+	viewportStyle := lipgloss.NewStyle()
+	if m.focus == focusMessages {
+		viewportStyle = viewportStyle.BorderForeground(lipgloss.Color("62"))
+	}
+
+	inputStyle := lipgloss.NewStyle().
+		Padding(0, 1)
+
+	if m.focus == focusInput {
+		inputStyle = inputStyle.BorderForeground(lipgloss.Color("62"))
+	}
+
+	return lipgloss.JoinVertical(
+		lipgloss.Left,
+		header,
+		viewportStyle.Render(m.viewport.View()),
+		inputStyle.Render(m.input.View()),
+	)
+}
+
+func (m *Model) updateViewportContent() {
+	var content string
+	for _, msg := range m.messages {
+		content += msg + "\n"
+	}
+	m.viewport.SetContent(content)
 }
